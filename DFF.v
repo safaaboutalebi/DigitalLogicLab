76e1079cb7adbfdfe39a80b4021d7b1cc55bb853
safaaboutalebi <44769936+safaaboutalebi@users.noreply.github.com>
2020-11-08 12:02:42 +0330

    Update DFF.v

--- DFF.v
+++ DFF.v
@@ -1,1 +1,18 @@
 
+module PEdge_DFlipFlop(D, clk, reset, Q, Qbar);
+
+    input D, clk, reset;
+    output reg Qbar;
+    output Q;
+
+    assign Q = ~Qbar;
+
+    always @(posedge clk)
+    begin
+        if (reset == 1'b1)
+            Qbar = 1'b1;
+        else
+            Qbar = ~D;
+    end
+
+endmodule
